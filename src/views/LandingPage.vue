b92ae4328a93c271931a1e9c9ceb6b1925efff17
Rangel <marcelo.rcs@icloud.com>
2022-08-11 00:33:08 -0300

    Create LandingPage.vue

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <NavBar></NavBar>
+    </div>
+</template>
+
+<script lang="ts">
+
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+    name: 'LandingPage',
+    components: { NavBar }
+}
+</script>
+
+<style scoped>
+.body {
+    font-family: Nunito,sans-serif;
+    font-size: .8rem;
+    font-weight: 400;
+    color: #212121;
+    background: #f8f8f8;
+}
+</style>
