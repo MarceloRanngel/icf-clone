71fd712abc15570f29b203bd00f1e5839e3bc711
Rangel <marcelo.rcs@icloud.com>
2022-08-11 06:54:40 -0300

    Add Footer

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,6 +1,138 @@
 <template>
     <div>
         <NavBar></NavBar>
+        <div>
+        <section class="slice">
+            <div class="container">
+                <div id="reverse" class="row">
+                    <div class="order-2">
+                        <figure>
+                            <img src="../assets/forma3d.png" class="img-fluid" />
+                            </figure>
+                    </div>
+                    <div class="order-1">
+                        <h1 class="text-style">
+                            Estamos transformando a forma de
+                            <strong class="text-primary">construir</strong> no Brasil.
+                        </h1>
+                        <p class="p-header">
+                            O maior grupo de empresas especializadas em construção com
+                            a tecnologia ICF da iForms.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="slice middle">
+        <div class="container">
+            <div class="card mb-4" style="background-color: rgb(38, 37, 38); color: white !important;">
+
+                <div class="card-body">
+
+                    <div id="cloud" class="feature">
+                        <picture class="mb-2 video">
+                            <img src="https://api.grupoicf.com.br/uploads/small_iforms_concrete_80c5fcc07e.png" class="img_semana">
+                        </picture>
+                        <div class="details">
+                            <div class="info">
+                                De 09 a 11 de Agosto
+                            </div>
+                            <h2 class="mb-4 big-title" style="color: white !important;">
+                                🔥Visite a iForms na Concrete Show SP 2022
+                            </h2>
+                            <p class="text-small" style="color: white !important;">
+                                📆 3 dias de
+                                Concrete Show São Paulo grátis - A maior feira de soluções em concrete da América
+                                Latina.
+
+                                Eeeita glória, a inovação na construção civil chegou, agora você pode conhecer
+                                presencialmente a tecnologia construtiva que está aquecendo o mercado🔥
+                                Nós estamos transformando a construção civil brasileira com obras 50% mais rápidas, alta
+                                qualidade térmica e acústica e resistência superior ao convencional.</p> <a
+                                href="https://eventos.grupoicf.com.br/" target="_blank"><span> <span class="label"
+                                        style="font-size: 16px; color: white !important; font-weight: 700;">Garantir
+                                        vaga
+                                    </span></span> <br></a><a
+                                href="https://eventos.grupoicf.com.br/localizacao-iforms-icf-na-concrete-show-sp/"><span>
+                                    <span class="label"
+                                        style="font-size: 16px; color: white !important; font-weight: 700;">Localização
+                                        do estande
+                                    </span></span> <br></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </section>
+        <section class="slice middle">
+         <div class="container">
+            <div class="card mb-4" style="background-color: rgb(255, 255, 255); color: rgb(38, 37, 38) !important;">
+                <div class="card-body">
+                    <div id="cloud" class="feature">
+                        <picture class="mb-2 video"><img
+                                src="https://api.grupoicf.com.br/uploads/medium_Logomarca_ICFLEX_5c68ba4b15.png"
+                                srcset="https://api.grupoicf.com.br/uploads/medium_Logomarca_ICFLEX_5c68ba4b15.png"
+                                alt="" class="img_semana"></picture>
+                        <div class="details mb-2">
+                            <div class="info">
+                                Descubra
+                            </div>
+                            <h2 class="mb-4 big-title" style="color: black !important;">
+                                A revolução no acabamento de obras com ICF
+                            </h2>
+                            <p class="text-small" style="color: black !important;">O ICFLEX é um
+                                acabamento 4 em 1, que combate fissuras e infiltrações e dispensa o uso de chapisco,
+                                reboco, textura e selador, solicite um orçamento no botão abaixo.</p> <a
+                             href="https://icfargamassas.com.br" target="_blank"><span
+                                 class="mb-2"><i
+                                        class="simple-icon-arrow-right-circle"></i> <span
+                                        class="label"
+                                        style="font-size: 16px; color: black !important; font-weight: 700;">Saiba mais
+                                    </span></span> <br></a><a
+                                href="https://icfargamassas.com.br/solicitar-orcamento" target="_blank"><span
+                                 class="mb-2"><i
+                                        class="simple-icon-energy"></i> <span style="font-size: 16px; color: black !important; font-weight: 700;">Solicitar
+                                        orçamento
+                                    </span></span> <br></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+        <footer data-v-2254b350="" id="footer-main" class="position-relative">
+        <div data-v-2254b350="" class="footer pt-lg-7 footer-dark card-green">
+            <div data-v-2254b350="" class="
+        shape-container
+        shape-line
+        shape-position-top
+        shape-orientation-inverse
+      "><svg data-v-2254b350="" width="2560px" height="100px" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px"
+                    viewBox="0 0 2560 100" xml:space="preserve" class="fill-section-secondary">
+                    <polygon data-v-2254b350="" points="2560 0 2560 100 0 100"></polygon>
+                </svg></div>
+            <div data-v-2254b350="" class="container pt-4">
+                <div data-v-2254b350="" class="row pl-4">
+                    <div data-v-2254b350="" class="col-lg-4 mb-5 mb-lg-0"><a data-v-2254b350="" href="#"><img
+                                data-v-2254b350="" alt="Image placeholder"
+                                src="https://api.grupoicf.com.br/uploads/BRANCA_FUNDO_ESCURO_8bc00e5031.png"
+                                id="footer-logo" class="img-fluid"></a>
+                        <p data-v-2254b350="" class="mt-4 text-sm pr-lg-4">
+                            O Grupo ICF tem como propósito transformar a forma com
+                            que as pessoas pensam na construção e facilitar a vida
+                            de quem habita ou executa obras com o sistema
+                            construtivo iForms.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+        
+        </div>
     </div>
 </template>
 
@@ -14,12 +146,398 @@ export default {
 }
 </script>
 
-<style scoped>
-.body {
-    font-family: Nunito,sans-serif;
+<style lang="css" scoped>
+div{
+    box-sizing: border-box;
+}
+.container {
+    width: 100%;
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 15px;
+    padding-left: 15px;
+    overflow-x: hidden;
+}
+
+.p-header{
+    color: #718096!important;
+    font-size: 1.125rem;
+    font-weight: 300;
+    margin-top: 0;
+    margin-bottom: 1rem;
+    text-align: left!important;
+}
+
+@media (min-width: 1200px) {
+.container{
+    max-width: 1410px;
+}
+}
+
+@media (max-width: 992px){
+.container{
+    max-width: 960px;
+}}
+
+@media (max-width: 768px){
+.container {
+    max-width: 720px;
+}}
+
+@media (max-width: 576px){
+.container {
+    max-width: 540px;
+}}
+
+
+.slice {
+    position: relative;
+    padding-top: 7.5rem;
+    padding-bottom: 4rem;
+}
+
+
+@media (max-width: 1200px) {
+    .text-style {
+        font-size: calc(1.425rem + 2.1vw);
+    }
+}
+
+.text-style {
+    font-size: 3.5rem;
+    font-weight: 700;
+    line-height: 1.3;
+    text-align: left !important;
+}
+
+@media (min-width: 768px) {
+    .order-1 {
+        order: 1!important;
+        max-width: 50% !important;
+        flex: 0 0 50% !important;
+
+    }
+}
+
+@media (min-width: 768px) {
+    .order-2 {
+        order: 2!important;
+        max-width: 41.66667% !important;
+        flex: 0 0 41.66667% !important;
+
+    }
+}
+
+@media (min-width: 992px) {
+    .order-2 {
+        order: 2!important;
+        max-width: 50% !important;
+        flex: 0 0 50% !important;
+    }
+}
+
+@media (min-width: 992px) {
+    .order-1 {
+        order: 1!important;
+        max-width: 50% !important;
+        flex: 0 0 50% !important;
+    }
+}
+
+.order-1 {
+    order: 2;
+    position: relative;
+    width: 100%;
+    max-width: 100%;
+    flex: 0 0 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+}
+
+.order-2 {
+    order: 1;
+    position: relative;
+    width: 100%;
+    max-width: 100%;
+    flex: 0 0 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+
+}
+
+
+section {
+    display: block;
+}
+
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+}
+
+.text-primary {
+    color: #54b082 !important;
+}
+
+b {
+    font-weight: 700;
+}
+
+
+
+img {
+    vertical-align: middle;
+    border-style: none;
+}
+
+@media (max-width: 768px) {
+#reverse {
+     display: flex;
+    flex-direction: column-reverse;
+}
+}
+
+#reverse {
+    box-sizing: border-box;
+}
+
+.img-fluid,
+.img-thumbnail {
+    max-width: 100%;
+    height: auto;
+}
+
+@media (min-width: 768px) {
+
+    .img-fluid,
+    .img-thumbnail {
+        max-width: 80% !important;
+        height: auto;
+    }
+}
+
+* {
+    box-sizing: border-box;
+}
+
+
+/*Daqui pra baixo*/
+body {
+    font-family: Nunito, sans-serif;
     font-size: .8rem;
     font-weight: 400;
     color: #212121;
     background: #f8f8f8;
+}
+
+
+.card{
+    border-radius: 0.75rem;
+}
+
+.card-body{
+    min-height: 1px;
+    padding: 1.75rem;
+    flex: 1 1 auto;
+}
+
+.feature:last-of-type {
+    margin-bottom: 0;
+}
+
+@media (max-width: 970px){
+.feature{
+    flex-direction: column-reverse;
+    text-align: center;
+    margin-bottom: 150px;
+}}
+
+.feature {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 200px;
+}
+
+.big-title, .tagline {
+    line-height: 1;
+}
+.big-title {
+    margin: 0;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 1.25;
+}
+.mb-4, .my-4{
+    margin-bottom: 1.5rem!important;
+}
+
+
+p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+}
+
+.text-small {
+    font-size: .76rem;
+    line-height: .9rem;
+}
+
+details {
+    max-width: 500px;
+}
+
+.img_semana {
+    width: 70%;
+    padding: 5%;
+}
+
+@media (max-width: 970px)
+{.info {
+    display: inline-flex!important;
+}}
+
+.info{
+    display: flex;
+    align-items: center;
+    margin-bottom: 24px;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 1.35;
+    color: #2ccb89;
+}
+
+
+img{
+    vertical-align: middle;
+    border-style: none;
+}
+
+@media (max-width: 970px)
+{.video {
+    margin-left: 0!important;
+    margin-right: 0!important;
+}}
+@media (max-width: 970px)
+{.video {
+    width: 70%;
+}}
+@media (max-width: 1350px)
+{.video {
+    width: 58%;}
+}
+.video {
+    width: 62%;
+    max-width: 600px;
+    height: auto;
+    flex-shrink: 1;
+    position: relative;
+}
+
+.mb-2, .my-2{
+    margin-bottom: 0.5rem!important;
+}
+
+.img_semana {
+    width: 70%;
+    padding: 5%;
+}
+
+@media (min-width: 1200px) {
+.card{
+    max-width: 1410px;
+}
+}
+
+@media (max-width: 992px){
+.card{
+    max-width: 960px;
+}}
+
+@media (max-width: 768px){
+.card {
+    max-width: 720px;
+}}
+
+@media (max-width: 576px){
+.card {
+    max-width: 540px;
+}}
+
+.middle{
+    padding-top: 0rem!important;
+    padding-bottom: 0rem!important;
+}
+
+
+.card-green {
+    background-color: #1c8968!important;
+}
+.footer-dark{
+    color: #fff;
+    background-color: #273444;
+}
+.footer {
+    padding-top: 3rem;
+    background-color: #fff;
+}
+
+.position-relative{
+    position: relative!important;
+}
+
+.shape-line {
+    min-height: 19px;
+}
+.shape-orientation-inverse{
+    transform: rotate(180deg);
+}
+.shape-position-top{
+    top: 0;
+    margin-top: -3px;
+}
+.shape-container {
+    position: absolute;
+    z-index: 2;
+    right: 0;
+    left: 0;
+    overflow: hidden;
+    width: 100%;
+}
+
+.pt-4{
+    padding-top: 1.5rem!important;
+}
+
+.fill-section-secondary[data-v-2254b350] {
+    fill: #fafbfe!important;
+}
+
+@media (min-width: 992px)
+{.col-lg-4[data-v-2254b350] {
+    max-width: 33.33333%;
+    flex: 0 0 33.33333%;
+}}
+
+.card-green span{
+    color: #fff!important;
+}
+
+.mb-5{
+    margin-bottom: 3rem!important;
+}
+
+.mt-4{
+    margin-top: 1.5rem!important;
+}
+
+.text-sm{
+    font-size: .875rem!important;
+}
+
+.pl-4{
+    padding-left: 3rem!important;
 }
 </style>
